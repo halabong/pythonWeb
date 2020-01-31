9098497ed7a01811499c26ac41488be9ec4b94f5
Hyun Kang <hyun@Hyunui-MacBookPro.local>
2020-01-31 16:36:39 +0900

    initial commit

---
+++ index.py
@@ -0,0 +1,54 @@
+#!/Library/Frameworks/Python.framework/Versions/3.8/bin/python3
+print("Content-Type: text/html")
+print()
+import cgi, os
+
+def getLlist():
+  files = os.listdir('data')
+  listStr = ''
+  for item in files:
+      listStr = listStr + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item)
+  return listStr
+
+form = cgi.FieldStorage()
+if 'id' in form:
+    pageId = form["id"].value
+    description = open('data/'+pageId, 'r').read()
+    description = description.replace('<', '&lt;')
+    description = description.replace('>', '&gt;')
+    update_link = '<a href="update.py?id={}">update</a>'.format(pageId)
+    delete_action = '''
+      <form action="process_delete.py" method="post">
+        <input type="hidden" name="pageId" value="{}">
+        <input type="submit" value="delete">
+      </form>
+    '''.format(pageId)
+else:
+    pageId = 'Welcome'
+    description = 'Hello, web'
+    update_link = ''
+    delete_action = ''
+print('''<!doctype html>
+<html>
+<head>
+  <title>WEB1 - Welcome</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="index.py">WEB</a></h1>
+  <ol>
+    {listStr}
+  </ol>
+  <a href="create.py">create</a>
+  <h2>{title}</h2>
+  <p>{desc}</p>
+    {update_link}
+    {delete_action}
+</body>
+</html>
+'''.format(
+    title=pageId, 
+    desc=description, 
+    listStr=getLlist(), 
+    update_link=update_link, 
+    delete_action=delete_action))
